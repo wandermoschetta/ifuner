98c0b5b0f53d82bef4cc711776734b27b76add3c
wander <wander@wsm.dev>
2020-07-23 00:30:26 -0300

    part of login updated

--- ifuner/core/views.py
+++ ifuner/core/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+
 
 def index(request):
     template_name = 'index.html'
     return render(request, template_name)
 
 def login(request):
-    template_name = 'login.html'
-    return render(request, template_name)
+    template_name = 'index.html'
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        print("email: {}  senha: {} ".format(username, password))
+
+        usuario = authenticate(request, username=username, password=password)
+        print("usuario ", usuario)
+        if usuario is not None:
+            # login(request, usuario)
+            print("usuario ", usuario)
+            form_login = "index"
+            return redirect('index')
+        else:
+           form_login = "login"
+
+    else:
+        form_login = "login"
+
+    return render(request, 'login.html', {'form_login': form_login})
+
+
--- ifuner/settings.py
+++ ifuner/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ifuner.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'ifuner',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
